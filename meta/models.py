6a85ed357e43cd42acdcf12035bf41a466b7fb0f
EricaCamelopardalis <clarajs2017@outlook.com>
2025-05-22 00:19:39 -0500

    Capitalized all the M in models.Model, added BrandCategory table

--- meta/models.py
+++ meta/models.py
@@ -2,27 +2,33 @@ from django.db import models
 
 
 # Create your models here.
-class Tone(models.model):
+class Tone(models.Model):
     ToneID = models.AutoField(primary_key=True)
     ToneName = models.CharField(max_length=8)
     ToneDescription = models.CharField(80)
 
 
-class Undertone(models.model):
+class Undertone(models.Model):
     UndertoneID = models.AutoField(primary_key=True)
     UndertoneName = models.CharField(max_length=12)
     UndertoneDescription = models.Charfield(80)
 
 
-class Opacity(models.model):
+class Opacity(models.Model):
     OpacityID = models.AutoField(primary_key=True)
     OpacityName = models.CharField(max_length=12)
     OpacityDescription = models.CharField(max_length=80)
 
 
-class Line(models.model):
+class Line(models.Model):
     LineID = models.AutoField(primary_key=True)
     BrandID = models.ForeignKey("core.Brand", on_delete=models.PROTECT)
     LineName = models.CharField(max_length=25)
     LineDescription = models.CharField(max_length=80)
     BottleSize = models.DecimalField(max_digits=4, decimal_places=1)
+
+
+class BrandCategory(models.Model):
+    BrandCategoryID = models.AutoField(primary_key=True)
+    BrandCategoryName = models.CharField(max_length=12)
+    BrandCategoryDescription = models.CharField(max_length=80)
