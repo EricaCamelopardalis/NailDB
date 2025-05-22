5bb1b36f19d4c15d04b50bd25c87bdd9d08f6555
EricaCamelopardalis <clarajs2017@outlook.com>
2025-05-22 00:15:07 -0500

    Added the Brand table to core models.py

--- core/models.py
+++ core/models.py
@@ -7,10 +7,10 @@ class Polish(models.Model):
     Description = models.CharField(max_length=80)
     Price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     BrandID = models.ForeignKey("Brand", on_delete=models.PROTECT)
-    ToneID = models.ForeignKey("Tone", on_delete=models.PROTECT)
-    UndertoneID = models.ForeignKey("Undertone", on_delete=models.PROTECT)
-    OpacityID = models.ForeignKey("Opacity", on_delete=models.PROTECT)
-    LineID = models.ForeignKey("Line", on_delete=models.PROTECT)
+    ToneID = models.ForeignKey("meta.Tone", on_delete=models.PROTECT)
+    UndertoneID = models.ForeignKey("meta.Undertone", on_delete=models.PROTECT)
+    OpacityID = models.ForeignKey("meta.Opacity", on_delete=models.PROTECT)
+    LineID = models.ForeignKey("meta.Line", on_delete=models.PROTECT)
     EventID = models.ForeignKey("Event", on_delete=models.SET_NULL, null=True)
     CollectionID = models.ForeignKey("Collection", on_delete=models.SET_NULL, null=True)
     IsDriesMatte = models.BooleanField(default=False)
@@ -18,3 +18,15 @@ class Polish(models.Model):
     IsUV = models.BooleanField(default=False)
     IsTopper = models.BooleanField(default=False)
     IsThermal = models.BooleanField(default=False)
+
+
+class Brand(models.model):
+    BrandID = models.AutoField(primary_key=True)
+    BrandName = models.CharField(max_length=25)
+    Country = models.CharField(max_length=20)
+    BrandCategoryID = models.ForeignKey("meta.BrandCategory", on_delete=models.PROTECT)
+    ParentCompany = models.CharField(max_length=25, null=True)
+    FoundingYear = models.IntegerField(null=True)  # Add validators eventually
+    OperatingCity = models.CharField(max_length=25, null=True)
+    OperatingRegion = models.CharField(max_length=25, null=True)
+    WebsiteUrl = models.CharField(max_length=2083, null=True)
