f78904b043adb11f50024b626e38c52241bbfd04
EricaCamelopardalis <clarajs2017@outlook.com>
2025-05-22 00:21:57 -0500

    Added schema prefixes to Event, Collection, and Brand to Polish table

--- core/models.py
+++ core/models.py
@@ -6,13 +6,15 @@ class Polish(models.Model):
     PolishID = models.AutoField(primary_key=True)
     Description = models.CharField(max_length=80)
     Price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    BrandID = models.ForeignKey("Brand", on_delete=models.PROTECT)
+    BrandID = models.ForeignKey("core.Brand", on_delete=models.PROTECT)
     ToneID = models.ForeignKey("meta.Tone", on_delete=models.PROTECT)
     UndertoneID = models.ForeignKey("meta.Undertone", on_delete=models.PROTECT)
     OpacityID = models.ForeignKey("meta.Opacity", on_delete=models.PROTECT)
     LineID = models.ForeignKey("meta.Line", on_delete=models.PROTECT)
-    EventID = models.ForeignKey("Event", on_delete=models.SET_NULL, null=True)
-    CollectionID = models.ForeignKey("Collection", on_delete=models.SET_NULL, null=True)
+    EventID = models.ForeignKey("core.Event", on_delete=models.SET_NULL, null=True)
+    CollectionID = models.ForeignKey(
+        "core.Collection", on_delete=models.SET_NULL, null=True
+    )
     IsDriesMatte = models.BooleanField(default=False)
     IsGlowIntheDark = models.BooleanField(default=False)
     IsUV = models.BooleanField(default=False)
@@ -20,7 +22,7 @@ class Polish(models.Model):
     IsThermal = models.BooleanField(default=False)
 
 
-class Brand(models.model):
+class Brand(models.Model):
     BrandID = models.AutoField(primary_key=True)
     BrandName = models.CharField(max_length=25)
     Country = models.CharField(max_length=20)
