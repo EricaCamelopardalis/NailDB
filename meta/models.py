969ca8af9ed058c142369f7afa2a4f181a7f78a1
EricaCamelopardalis <clarajs2017@outlook.com>
2025-05-21 23:53:06 -0500

    Added the Tone table to models.py in meta app

--- meta/models.py
+++ meta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Tone(models.model):
+    ToneID = models.AutoField(primary_key=True)
+    ToneName = models.CharField(max_length=8)
+    ToneDescription = models.CharField(80)
